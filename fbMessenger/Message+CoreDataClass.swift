c0d22a03771da6eba08d845ea44e0cbcead2cd41
Michael Lema <Mel526@nyu.edu>
2017-07-22 23:26:02 -0400

    Updated ReadMe

---
+++ fbMessenger/Message+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Message+CoreDataClass.swift
+//  fbMessenger
+//
+//  Created by Michael Lema on 7/22/17.
+//  Copyright © 2017 Michael Lema. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Message)
+public class Message: NSManagedObject {
+
+}
